85efae6ed391829fa3db74a39736f0b2bc98a5bc
habiba zainab <habibazainabali@gmail.com>
2026-09-05 13:39:47 -0700

    Q1 with code

--- 01-basic-work.py
+++ 01-basic-work.py
@@ -1,9 +1,20 @@
 """
 
 ===========================================================
-   LECTURE 01 - SET 01 : BASICS
+   LECTURE 01 - SET 01 : BASICS OF PYTHON
    Topics : Character Set, Variables, Rules of Identifiers
    Total Questions :
 ============================================================
 
 """
+# ==========================================================
+# PART A:   CHARACTER SET
+# ==========================================================
+
+# Q1: Print a simple Python banner using letters, digits and
+#     special characters.
+
+print("=== WELCOME TO PYTHON ===  >>>  LET'S CODE!")
+
+# ----------------------------------------------------------
+
