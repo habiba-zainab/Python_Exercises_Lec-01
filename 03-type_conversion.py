c98d2df66920fe9b8f6db840288329ea8ce7442e
habiba zainab <habibazainabali@gmail.com>
2026-09-08 12:13:08 -0700

    Questioon 1 of type conversion

--- 03-type_conversion.py
+++ 03-type_conversion.py
@@ -12,4 +12,9 @@
 # PART A:    TYPE CONVERSION (IMPLICIT)
 # ==========================================================
 
-# 
+# Q1: Divide two Integers.
+#    a = 10 , b = 4
+#    Find c = a / b , print c and type(c)
+#    Also try: 10 / 2 and see the type.
+#    (HINT:  / always gives float)
+
