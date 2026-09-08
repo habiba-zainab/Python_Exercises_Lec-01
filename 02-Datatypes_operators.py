40295e56d069ec39a24427906a65e3d394204df0
habiba zainab <habibazainabali@gmail.com>
2026-09-08 08:01:09 -0700

    Q1 TYPES

--- 02-Datatypes_operators.py
+++ 02-Datatypes_operators.py
@@ -15,3 +15,16 @@
 #    int, float, str, bool, None
 #    Print the Value and the Type of each one.
 
+my_int = 42
+my_float = 3.14
+my_str = "Welcome"
+my_bool = True
+my_none = None
+
+print("INTEGER: ", my_int, "| Type: ", type(my_int))
+print("FLOAT: ", my_float, "| Type: ", type(my_float))
+print("STRING: ", my_str, "| Type: ", type(my_str))
+print("BOOLEAN: ", my_bool, "| Type: ", type(my_bool))
+print("NONE: ", my_none, "| Type: ", type(my_none))
+
+# ----------------------------------------------------------
