ec4492a7b3263f9a13453c72dfe699e9ad8eea9e
habiba zainab <habibazainabali@gmail.com>
2026-09-09 05:38:02 -0700

    Step 03 Price & Quantities

--- 04-Challenges.py
+++ 04-Challenges.py
@@ -28,7 +28,11 @@ item1_name = "Laptop"
 item2_name = "Mouse"
 item3_name = "Keyboard"
 
-
 print(f"\nShop: {shop_name}")
 print(f"Customer: {customer_name}")
 print(f"Customer ID: {customer_id}")
+
+# ----------------------------------------------------------
+#     STEP 03:    Set Prices & Quantities
+# ----------------------------------------------------------
+
